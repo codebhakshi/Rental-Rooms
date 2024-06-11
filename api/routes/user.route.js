b28b97458716f9d882f999d750b7d6018acd7a88
UmeshBirla01 <umeshbirla2001@gmail.com>
2024-06-11 14:15:06 +0530

    create sign up api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,10 +3,5 @@ import {test} from '../controllers/user.controller.js';
 const router = express.Router();
 
 router.get('/test',test);
-// router.get("/test", (req,res)=>{
-//     res.json({
-//         msg:'Api is Working....',
-//     });
-// });
 
 export default router;
