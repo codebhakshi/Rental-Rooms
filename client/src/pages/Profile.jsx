a784cbbe507764613e21c98cc0b6ef4317496416
UmeshBirla01 <umeshbirla2001@gmail.com>
2024-06-12 19:59:57 +0530

    create listing page functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,12 +14,12 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
-  signOutUserFailure,
-  signOutUserSuccess,
+  // signOutUserFailure,
+  // signOutUserSuccess,
   signOutUserStart,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 export default function Profile() {
   const fileRef = useRef(null);
   const { currentUser, loading, error } = useSelector((state) => state.user);
@@ -221,12 +221,12 @@ export default function Profile() {
         >
           {loading ? 'Loading...' : 'Update'}
         </button>
-        {/* <Link
+        <Link
           className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
           to={'/create-listing'}
         >
           Create Listing
-        </Link> */}
+        </Link>
       </form>
       <div className='flex justify-between mt-5'>
         <span
